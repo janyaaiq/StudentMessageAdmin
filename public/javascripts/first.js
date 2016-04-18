5ca6b0623927d467ea70f50f5b60b5aeff3c8527
jokerdrake <joker92919@gmail.com>
2016-04-18 10:45:45 +0800

    基本表格建完

--- public/javascripts/first.js
+++ public/javascripts/first.js
@@ -2,22 +2,72 @@ var MyTable = React.createClass({
   getInitialState:function(){
     return{
       filter: 'fff',
+      tableMessage: [],
     };
   },
   render: function(){
     return (
     <div>
-      <div><span>filter:</span><input value={this.state.filter} onChange={(e)=>this.setFilter(e)}/><button onClick={()=>this.showFilter()}>获取</button></div>
-      <div></div>
+      <div><span>条件:</span><input value={this.state.filter} onChange={(e)=>this.setFilter(e)}/><button onClick={()=>this.showFilter()}>查询</button><button onClick={()=>this.getAllMessage()}>获取全部信息</button></div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <td>姓名</td>
+              <td>性别</td>
+              <td>年龄</td>
+              <td>游戏ID</td>
+              <td>S6赛季排位段位</td>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.tableMessage.length > 0 && this.state.tableMessage.map((child,index)=>
+                <tr key={index}>
+                  <td>{child.name}</td>
+                  <td>{child.sex}</td>
+                  <td>{child.age}</td>
+                  <td>{child.ID}</td>
+                  <td>{child.duanwei}</td>
+                </tr>
+              )
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
     );
+  },
+  componentDidMount:function(){
+    
+  },
+  componentWillMount:function(){
+    
+  },
+  componentWillReceiveProps:function(){
+    
   },
   setFilter:function(e){
     this.setState({filter: e.target.value});
   },
   showFilter:function(){
     alert(this.state.filter);
-  }
+  },
+  getAllMessage:function(){
+    var me = this;
+    $.ajax({
+      url: '/admin/get_all_message',
+      type: 'get',
+      dataType: 'json',
+      data: '',
+      success:function(data){
+       me.setState({tableMessage: data.allMessage});
+      },
+      error:function(msg){
+        console.log(msg);
+      },
+    });
+  },
 });
 
 ReactDOM.render(<MyTable></MyTable>, document.getElementById('container'));
--- routes/admin.js
+++ routes/admin.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('stu_msg_admin', { title: 'Student Message Admin' });
 });
 
+router.get('/get_all_message', function(req, res, next){
+  var allMessage = [
+    {name: '李惠康', sex: '男', age: '21', duanwei: '青铜五', ID: '冷咖啡'},
+    {name: '冷恒瑞', sex: '男', age: '21', duanwei: '青铜五', ID: '鸟大才是王道'},
+    {name: '袁凡', sex: '男', age: '21', duanwei: '青铜五', ID: '猫寻欢'},
+    {name: '娄坤恒', sex: '男', age: '21', duanwei: '青铜五', ID: '雪粒儿'},
+    {name: '同行', sex: '男', age: '21', duanwei: '青铜五', ID: '妄言之SB'},
+    {name: '李童', sex: '男', age: '21', duanwei: '青铜五', ID: '不记得'},
+    {name: '大禽', sex: '男', age: '21', duanwei: '青铜五', ID: '流言蜚语'},
+    {name: '何浪', sex: '男', age: '21', duanwei: '最强王者', ID: 'Lvily...Alberte'},
+  ];
+  
+  res.json({allMessage: allMessage});
+});
 module.exports = router;
